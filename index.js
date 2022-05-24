7539245c60cde278dfaee68daf1a61f1e2df6509
forhadbinamir <forhadbinamir@gmail.com>
2022-05-25 02:48:53 +0300

    JWT create

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ app.use(express.json())
 
 // user: manufacturer
 // pass : TxGdW9ZxMBEpfMPF
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Access Forbidden' })
+        }
+        req.decoded = decoded
+        console.log(decoded)
+        next()
+    })
 
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.ACCESS_SECRET_PASS}@cluster0.y263y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -38,6 +53,40 @@ async function run() {
         app.post('/orders', async (req, res) => {
             const query = req.body
             const result = await orderCollection.insertOne(query)
+            res.send(result)
+        })
+        // app.post('/orders', async (req, res) => {
+        //     const query = req.body
+        //     const result = await userCollection.insertOne(query)
+        //     res.send(result)
+        // })
+
+        app.put('/user/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    user
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const accessToken = jwt.sign({ email: email }, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1d' })
+            res.send({ result, accessToken })
+        })
+
+        app.put('/update/:id', async (req, res) => {
+            const id = req.params.id
+            const updateQuantity = req.body
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                }
+            }
+            const result = await manufacturerCollection.updateOne(filter, updateDoc, options)
             res.send(result)
         })
     }
